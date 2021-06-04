38ecbb9d0ff01f03721354f352802ee3b3d2ba4b
‪rahma alnassar‬‏ <rahmaalnassar231@gmail.com>
2021-06-04 18:00:01 +0300

    crud courses last update

--- resources/views/backend/courses/edit.blade.php
+++ resources/views/backend/courses/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.backend')
 
+@section('style')
+<link rel="stylesheet" href="{{path('vendors/css/editors/summernote.css')}}">
+@endsection
+
 @section('content')
 <div class="content-body">
     
@@ -12,9 +16,9 @@
 </div>
 @endsection
 @section('script')
-    <link rel="stylesheet" href="{{path('vendors/css/editors/summernote.css')}}">
+    
     <script type="text/javascript" src="{{path('vendors/js/editors/summernote/summernote.js')}}"></script>
     <script>
-        $(document).ready(function() { $('.summernote').summernote();})
+        $(document).ready(function() { $('.summernote').summernote();});
     </script>
 @endsection
